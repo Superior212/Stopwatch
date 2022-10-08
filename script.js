33f501914f3b48cdc924df9e518d0692d956193e
Samson Aderonmu <94833173+Superior212@users.noreply.github.com>
2022-10-08 22:00:14 +0100

    update

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ const start = () => {
   let hourNotice  = new Audio ("audio/alarm-clock-beep.wav");
   let minuteNotic = new Audio("audio/clock-alarm-beep.wav");
 
-    let myMileSeconds = ms.value;
+    let myMileSeconds = milliSeconds.value;
     myMileSeconds++;
-    ms.value = myMileSeconds;
+    milliSeconds.value = myMileSeconds;
    
    setTime = setTimeout(start, "9");
 
-   if (ms.value==100) {
-    ms.value = 0;
-    ss.value++;
-   }else if (ss.value ==59) { 
-      ss.value = 00;
-      mm.value++;
+   if (milliSeconds.value==100) {
+    milliSeconds.value = 0;
+    seconds.value++;
+   }else if (seconds.value ==59) { 
+    seconds.value = 00;
+    minutes.value++;
       minuteNotic.play();
-    } else if (mm.value == 60) {
-      mm.value = 0;
-      hh.value++;
+    } else if (minutes.value == 60) {
+      minutes.value = 0;
+      hours.value++;
       hourNotice.play();
-    }else if (hh.value == 24) {
-        hh.value= 0;
-        dd.value++;
+    }else if (hours.value == 24) {
+      hours.value= 0;
+      days.value++;
         dayNotice.play();
     };
   };
@@ -32,8 +32,9 @@ const start = () => {
 
   const stopTime = () => {
     clearTimeout(setTime);
-    ss.value="";
-    mm.value="";
-    hh.value="";
-    dd.value="";
+    milliSeconds.value="";
+    seconds.value="";
+    minutes.value="";
+    hours.value="";
+    days.value="";
   };
